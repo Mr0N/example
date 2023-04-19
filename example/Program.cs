19451c56777e7b00a327f561fd2344bf056b2981
user <user@DESKTOP-2ERAVDP>
2023-04-19 22:57:07 +0300

    Add project files.

---
+++ example/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Numerics;
+
+Math.Sqrt((byte)21);
+class Math
+{
+    public static T Sqrt<T>(T value) where T:INumber<T>
+    {
+        throw new Exception();
+    }
+}
